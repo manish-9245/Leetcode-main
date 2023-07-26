318df035b73e0ef9fb7625e18555dd76c483e002
Manish Tiwari <69393822+manish-9245@users.noreply.github.com>
2023-07-27 00:07:58 +0530

    Time: 12 ms (99.15%), Space: 13.4 MB (59.10%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -5,19 +5,24 @@
 #         self.next = next
 class Solution(object):
     def rotateRight(self, head, k):
-        """
-        :type head: ListNode
-        :type k: int
-        :rtype: ListNode
-        """
-        if not head: return head
+        if not head or k == 0:
+            return head
+
+    # Find the length of the linked list and locate the last node
         length, tail = 1, head
         while tail.next:
             tail = tail.next
             length += 1
+
+    # Create a cycle and adjust k to get the effective rotation count
         tail.next = head
-        for _ in range(length - k % length):
-            tail = tail.next
-        new_head = tail.next
-        tail.next = None
+        k %= length
+
+    # Find the new tail node after rotation
+        for _ in range(length - k - 1):
+            head = head.next
+
+        new_head = head.next
+        head.next = None
+
         return new_head
