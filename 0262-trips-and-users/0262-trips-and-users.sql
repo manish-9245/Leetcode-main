5085d3cde3c7b64df1e44a45657f52680f6a2e91
Manish Tiwari <69393822+manish-9245@users.noreply.github.com>
2023-08-19 17:03:17 +0530

    Time: 1348 ms (38.31%), Space: 0B (100.00%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -5,9 +5,9 @@ from
 select id, A.client_id, B.banned as client_ban, A.driver_id, C.banned as driver_ban, status, request_at
 from Trips A
 left join
-(select * from Users where role = 'client') B
-on A.client_id = B.users_id 
+Users B
+on A.client_id = B.users_id and B.role = 'client'
 left join
-(select * from Users where role = 'driver') C
-on A.driver_id = C.users_id) A where client_ban = 'No' and driver_ban ='No' and
+Users C
+on A.driver_id = C.users_id and C.role = 'driver') A where client_ban = 'No' and driver_ban ='No' and
 (request_at between '2013-10-01' and '2013-10-03') group by Day order by Day;
