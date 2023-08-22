b5008c3337069c449510c0f1ba7f62e929bf2e5b
Manish Tiwari <69393822+manish-9245@users.noreply.github.com>
2023-08-22 23:43:13 +0530

    Time: 412 ms (68.62%), Space: 43.2 MB (60.95%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.py
+++ 1609-even-odd-tree/1609-even-odd-tree.py
@@ -1,20 +1,13 @@
 class Solution:
     def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
-        queue = [root]
-        level = 0
-        while queue:
-            prev_val = None
-            level_size = len(queue)
-            for _ in range(level_size):
-                node = queue.pop(0)
-                val = node.val
-                if (level % 2 == 0 and (val % 2 == 0 or (prev_val is not None and val <= prev_val))) or \
-                   (level % 2 == 1 and (val % 2 == 1 or (prev_val is not None and val >= prev_val))):
+        bit_level = 1
+        stack = [root] if root else []
+        while stack:
+            for i in range(len(stack)):
+                if stack[i].val & 1 != bit_level: 
                     return False
-                prev_val = val
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            level += 1
+                if i < len(stack) - 1 and ((stack[i].val > stack[i+1].val) == bool(bit_level) or stack[i].val == stack[i+1].val):
+                    return False
+            bit_level ^= 1
+            stack = [node for n in stack for node in (n.left, n.right) if node]
         return True
