9ed09f836d84217f870a70555507c818e5b413be
Manish <69393822+manish-9245@users.noreply.github.com>
2024-06-09 22:24:08 +0530

    Time: 34 ms (72.21%), Space: 16.5 MB (60.22%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.py
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -1,3 +1,6 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        return sum(abs(ord(a) - ord(b)) for a,b in pairwise(s))
+        total = 0
+        for i in range(len(s) - 1):
+            total += abs(ord(s[i]) - ord(s[i + 1]))
+        return total
